f8c428547d32fb20c3499929efb1d5826db92b00
Omar Abi Farraj <omar.7tech@gmail.com>
2026-07-05 06:55:56 +0300

    make min h for the image

--- resources/js/pages/home/sections/hero.tsx
+++ resources/js/pages/home/sections/hero.tsx
@@ -1,7 +1,7 @@
 function Hero() {
   return (
     <div>
-      <img className="w-full h-[80dvh] object-cover" src="/images/potted-plant-table.webp" alt="" />
+      <img className="w-full min-h-[80dvh] object-cover" src="/images/potted-plant-table.webp" alt="" />
     </div>
   )
 }
