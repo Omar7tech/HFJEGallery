b3641becfb03339698a6fdaa095ff6a93deaf1c7
Omar Abi Farraj <omar.7tech@gmail.com>
2026-09-16 23:47:28 +0300

    add sluggable

--- app/Models/LivingSpace.php
+++ app/Models/LivingSpace.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\Sluggable\Attributes\Sluggable;
 
 #[Fillable(['name', 'slug', 'icon', 'image_path', 'sort_order', 'is_active'])]
+#[Sluggable(from: 'name', to: 'slug')]
 class LivingSpace extends Model
 {
     /** @return array<string, string> */
